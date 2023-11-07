e7e3f5c48a71b502aef44b58e3d622a336e1a06a
Shridhar Jadhav <jadhavshridhar787@gmail.com>
2023-11-07 21:04:37 +0530

    Added new class

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Shri!");
+
+            MyClass myClass = new MyClass();
+            int j = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+            int n = myClass.MySum(j);
+            Console.WriteLine(n);
+            
+            Console.WriteLine();
         }
     }
 }
