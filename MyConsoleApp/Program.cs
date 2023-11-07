e6fd22df238ccc1bf84b04db93dc2d1c54432a07
Shridhar Jadhav <jadhavshridhar787@gmail.com>
2023-11-07 21:12:51 +0530

    Added new code

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Shri!");
+            for(int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
         }
     }
 }
