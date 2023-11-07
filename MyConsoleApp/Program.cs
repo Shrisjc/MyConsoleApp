3b9bd710628b3fcfbd7d7b6a7b4fdb0d5f655a64
Shridhar Jadhav <jadhavshridhar787@gmail.com>
2023-11-07 17:41:03 +0530

    Update access modifer

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace MyConsoleApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Shri!");
         }
     }
 }
